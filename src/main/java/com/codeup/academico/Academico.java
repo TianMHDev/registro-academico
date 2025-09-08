a08e8e140d4c5bb3c3ecd8aad9dc29cc6bd5710e
Sebastián Marriaga <hado6529@gmail.com>
2025-09-08 18:27:45 -0500

    Primer commit en develop

---
+++ src/main/java/com/codeup/academico/Academico.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.codeup.academico;
+
+/**
+ *
+ * @author sebastian
+ */
+public class Academico {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
