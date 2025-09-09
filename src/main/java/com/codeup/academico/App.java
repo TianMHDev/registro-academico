fed25092368f33ca87439a8ca085c0e1b4570658
Sebastián Marriaga <hado6529@gmail.com>
2025-09-08 19:42:28 -0500

    Primer commit

--- src/main/java/com/codeup/academico/App.java
+++ src/main/java/com/codeup/academico/App.java
@@ -1,16 +1,15 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 package com.codeup.academico;
 
 /**
  *
  * @author sebastian
  */
-public class Academico {
-
+public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Sistema Académico CodeUp iniciado correctamente 🚀");
     }
 }
